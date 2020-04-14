ed4d32f0f5618c5dfe77c0a401cb2210766c6aa5
Anton Kibekin <35627117+KiberTon@users.noreply.github.com>
2020-04-14 09:42:33 +0300

    Update ApiService.java

--- app/src/main/java/com/example/googlesearch/api/ApiService.java
+++ app/src/main/java/com/example/googlesearch/api/ApiService.java
@@ -8,7 +8,7 @@ import retrofit2.http.GET;
 
 public interface ApiService {
 
-    @GET("v1?key=AIzaSyCCgwiRwhUUG_-B6cStmymaKN59a3Wu-Cc&cx=013909217334106613332:oq3okfe7n4v" + "&q=Apple")
+    @GET(")
     Observable<Items> getItems();
 
 
